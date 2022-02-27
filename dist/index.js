f6e98b62cd0bb68e065cfdd9a92b0d31fcf374a7
Rahmat Agung Julians <rahmatagungj@gmail.com>
2022-02-27 20:11:36 +0700

    test prod file

--- dist/index.js
+++ dist/index.js
@@ -6,22 +6,16 @@ function forEach(items, callback) {
   }
 }
 
-let tes = owntestMock.fn(xs => {
-  let toReturn = []
-
-  xs.forEach(x => {
-    toReturn.push(x ** x);
-  });
-
-  return toReturn;
-})
+let tes = owntestMock.fn(x => x * 2)
 
 let userLocalStorage = owntestMock.localStorage();
 
-let user = [[1, 2, 3], [4, 5, 6]];
+let user = [1,2,3]
 
 userLocalStorage.setItem('user', JSON.stringify(user));
 
 forEach(JSON.parse(userLocalStorage.getItem('user')), tes);
 
 console.log(tes.mock);
+
+console.log(tes.mock.calls[1][0]);
